77ec52246d47191fd48ce17a1271e8d0ae2c2cfd
Iamneo <techadmin@iamneo.ai>
2023-09-15 11:30:14 +0000

    Backup Commit

--- operators.ps1
+++ operators.ps1
@@ -1,16 +1,10 @@
 #Operators
 
 #Assignment operators
-$x = 5  #Basic assignment
-$x += 5 #Add value to variable and reset the variable
-$x -= 5 #Subtract value from variable and reset the variable
-$x *= 5 #Muliply value by variable and reset the variable
-$x /= 5 #Divide variable by value and reset the variable
+$num = 5  #Basic assignment
+$num += 5 #Add value to variable and reset the variable
+$num -= 5 #Subtract value from variable and reset the variable
+$num *= 5 #Muliply value by variable and reset the variable
+$num /= 5 #Divide variable by value and reset the variable
 
-#Arithmetic operators
-5+5
-10-5
-10*5
-10/5
-
-$x
+$num
