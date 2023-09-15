ceb3b7226fabd09910980eed675126b209216047
Iamneo <techadmin@iamneo.ai>
2023-09-15 11:20:15 +0000

    Backup Commit

--- arrays.ps1
+++ arrays.ps1
@@ -2,12 +2,12 @@
 $OS = @('linux', 'windows', 'macos', 'unix')
 
 #Getting a value based on the index
-Write-Host $OS[0]
+#Write-Host $OS[0]
 #Write-Host $OS[1]
 #Write-Host $OS[2]
-Write-Host $OS[3]
+#Write-Host $OS[3]
 
 #Add a value to the array
-#$Jedi += "Qui-Gon Jin"
+$OS += "Andriod","IoS"
 
-#$OS
+$OS
