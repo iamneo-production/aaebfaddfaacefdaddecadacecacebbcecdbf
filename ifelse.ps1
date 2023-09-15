b5b4a6ec395d2f8ac44b3209d216bf753ae38808
Iamneo <techadmin@iamneo.ai>
2023-09-15 12:00:14 +0000

    Backup Commit

---
+++ ifelse.ps1
@@ -0,0 +1,8 @@
+#If then else example
+$x = 
+
+if ($x -gt 20) {
+    Write-Host "x is greater than 20"
+} else {
+    Write-Host "x is not greater than 20"
+}
---
+++ ifelse2.ps1
@@ -0,0 +1,11 @@
+#If then else elsif example
+
+$x = 10
+
+if ($x -gt 20) {
+    Write-Host "x is greater than 20"
+} elseif ($x -eq 10) {
+    Write-Host "x is equal to 10"
+} else {
+    Write-Host "x is not greater than 20 and not equal to 10"
+}
