eb4a8d5da197980a5fbeca25090db49e8e6270b3
Iamneo <techadmin@iamneo.ai>
2023-09-15 11:50:14 +0000

    Backup Commit

--- age.ps1
+++ age.ps1
@@ -3,4 +3,8 @@ $Month = Read-Host "Enter your Month of Birth (MM)"
 $Year = Read-Host "Enter your Year of Birth (YYYY)"
 
 #Age Calculation
-$currentyear = 
+$Age = 2023 - $Year
+$Age1 = 09 - $Month
+$Age2 = 15 - $Date
+
+Write-Host "Your age is $Age years $Age1 months $Age2 days"
