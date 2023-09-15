f168e804e827724b5ccfc612af5d05bb7b2b48ef
Iamneo <techadmin@iamneo.ai>
2023-09-15 11:45:14 +0000

    Backup Commit

---
+++ age.ps1
@@ -0,0 +1,6 @@
+$Date = Read-Host "Enter your Date of Birth (DD)"
+$Month = Read-Host "Enter your Month of Birth (MM)"
+$Year = Read-Host "Enter your Year of Birth (YYYY)"
+
+#Age Calculation
+$currentyear = 
