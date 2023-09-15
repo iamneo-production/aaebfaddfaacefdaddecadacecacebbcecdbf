0ab302cf0773d5ae0e3b61d8b092c26943eb0875
Iamneo <techadmin@iamneo.ai>
2023-09-15 11:15:14 +0000

    Backup Commit

---
+++ arrays.ps1
@@ -0,0 +1,13 @@
+#An example array
+$OS = @('linux', 'windows', 'macos', 'unix')
+
+#Getting a value based on the index
+Write-Host $OS[0]
+#Write-Host $OS[1]
+#Write-Host $OS[2]
+Write-Host $OS[3]
+
+#Add a value to the array
+#$Jedi += "Qui-Gon Jin"
+
+#$OS
